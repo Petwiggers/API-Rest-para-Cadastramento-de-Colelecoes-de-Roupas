c2008da11ef53c937845ed76dea1aa102ec583d4
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-06-14 19:21:34 -0300

    Criação Classe Pessoa

--- Projeto Avaliativo Módulo 02/Models/Pessoa.cs
+++ Projeto Avaliativo Módulo 02/Models/Pessoa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,11 @@ namespace Projeto_Avaliativo_Módulo_02.Models
 {
     public class Pessoa
     {
-        public 
+        public  int Id { get; set; }
+        public string NomeCompleto { get; set; }
+        public string Genero { get; set; }
+        public DateTime Data { get; set; }
+        public string Cpf_Cnpj { get; set; }
+        public string NumeroTelefone { get; set; }
     }
 }

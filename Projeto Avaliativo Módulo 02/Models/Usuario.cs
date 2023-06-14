7be7d057175fd2d2d5544240ad824888c8f5ace3
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-06-14 19:26:53 -0300

    Criação da Model Usuario

---
+++ Projeto Avaliativo Módulo 02/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Avaliativo_Módulo_02.Models
+{
+    public class Usuario : Pessoa
+    {
+        public string Email { get; set; }
+        public int TipoUsuario { get; set; }
+        public int Status { get; set; }
+    }
+}

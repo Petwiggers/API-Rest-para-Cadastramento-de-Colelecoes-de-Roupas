f68710c03e059ebdaf6abce1ca9fa5bdf1499e23
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-06-14 20:18:42 -0300

    Criação da Controller Usuario

---
+++ Projeto Avaliativo Módulo 02/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Projeto_Avaliativo_Módulo_02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Avaliativo_Módulo_02.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Usuario>> BuscarTodosUSuarios()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Projeto Avaliativo Módulo 02/Data/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projeto_Avaliativo_Módulo_02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Avaliativo_Módulo_02.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        
+    }
+}

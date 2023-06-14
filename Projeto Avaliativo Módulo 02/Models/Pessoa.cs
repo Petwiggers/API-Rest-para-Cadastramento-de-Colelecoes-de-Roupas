94545c1c186a0cb7f9df0bd7b0dc8764654a1822
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-06-14 11:48:48 -0300

    Criação Do Projeto

---
+++ Projeto Avaliativo Módulo 02/Models/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Avaliativo_Módulo_02.Models
+{
+    public class Pessoa
+    {
+        public 
+    }
+}

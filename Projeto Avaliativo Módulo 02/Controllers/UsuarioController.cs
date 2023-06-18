ea01b4ced751ba33d9de12760e9aa4b2f8a63e29
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-06-18 18:02:06 -0300

    Criação do Método Put Usuarios

--- Projeto Avaliativo Módulo 02/Controllers/UsuarioController.cs
+++ Projeto Avaliativo Módulo 02/Controllers/UsuarioController.cs
@@ -9,8 +9,8 @@ using System.Threading.Tasks;
 
 namespace Projeto_Avaliativo_Módulo_02.Controllers
 {
-    
-    [Route("api/[controller]")]
+
+    [Route("api/usuarios")]
     [ApiController]
     public class UsuarioController : ControllerBase
     {
@@ -29,7 +29,6 @@ namespace Projeto_Avaliativo_Módulo_02.Controllers
         }
 
         [HttpPost]
-        [Route("/Cliente")]
         public IActionResult CadastrandoUsuario([FromBody]Usuario usuario)
         {
             try
@@ -37,7 +36,6 @@ namespace Projeto_Avaliativo_Módulo_02.Controllers
                 Usuario validandoUsuario = _repository.Usuarios.FirstOrDefault(x => x.Cpf_Cnpj == usuario.Cpf_Cnpj);
                 if (validandoUsuario == null)
                 {
-
                     if ((usuario.Status == Enums.Status.Ativo || usuario.Status == Enums.Status.Inativo)
                         && (usuario.TipoUsuario == Enums.TipoUsuario.Administrador || usuario.TipoUsuario == Enums.TipoUsuario.Criador
                         || usuario.TipoUsuario == Enums.TipoUsuario.Gerente || usuario.TipoUsuario == Enums.TipoUsuario.Outro))
@@ -48,21 +46,49 @@ namespace Projeto_Avaliativo_Módulo_02.Controllers
                         {
                             return StatusCode(201, usuario);
                         }
-
                     }
                     return BadRequest("A algum erro na inserção de Dados");
-
                 }
-
                 return Conflict();
             }
             catch (Exception exception)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, exception.Message);
             }
-        
         }
 
-     
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult AtualizandoRegistro([FromBody]Usuario novoUsuario,[FromRoute] int id)
+        {
+            try
+            {   
+                if(novoUsuario.Id != id)
+                {
+                    return BadRequest("No corpo do Usuario você deve inserir o mesmo Id correspondente a ele!");
+                }
+                Usuario usuario = _repository.Usuarios.Find(id);
+                if(!(usuario == null))
+                {
+                    if ((novoUsuario.Status == Enums.Status.Ativo || novoUsuario.Status == Enums.Status.Inativo)
+                    && (novoUsuario.TipoUsuario == Enums.TipoUsuario.Administrador || novoUsuario.TipoUsuario == Enums.TipoUsuario.Criador
+                    || novoUsuario.TipoUsuario == Enums.TipoUsuario.Gerente || novoUsuario.TipoUsuario == Enums.TipoUsuario.Outro))
+                    {
+                        _repository.Entry(usuario).CurrentValues.SetValues(novoUsuario);
+                        int resultado = _repository.SaveChanges();
+                        if (resultado > 0)
+                        {
+                            return Ok(usuario);
+                        }
+                    }
+                    return BadRequest("A algum erro na inserção de Dados");
+                }
+                return NotFound("O Usuario informado não existe !");
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, exception.Message);
+            }
+        }
     }
 }
